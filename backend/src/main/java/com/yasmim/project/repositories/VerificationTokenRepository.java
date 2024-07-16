0124d97fc9f3165ab49a45a9ed199a005bd1202e
yasmi <yas1cf1@gmail.com>
2024-07-16 15:29:10 -0300

    email verification implemented

--- backend/src/main/java/com/yasmim/project/repositories/VerificationTokenRepository.java
+++ backend/src/main/java/com/yasmim/project/repositories/VerificationTokenRepository.java
@@ -1,7 +1,10 @@
 package com.yasmim.project.repositories;
 
+import com.yasmim.project.entities.UserData;
 import com.yasmim.project.entities.VerificationTokenData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VerificationTokenRepository extends JpaRepository<VerificationTokenData, Long> {
+    public VerificationTokenData findByToken(String token);
+    public VerificationTokenData deleteAllByUser(UserData user);
 }
--- backend/src/main/java/com/yasmim/project/services/UserService.java
+++ backend/src/main/java/com/yasmim/project/services/UserService.java
@@ -4,12 +4,17 @@ import com.yasmim.project.dto.AuthToken;
 import com.yasmim.project.dto.LoginData;
 import com.yasmim.project.dto.RegisterData;
 import com.yasmim.project.entities.UserData;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
     public AuthToken signin(LoginData obj);
     public AuthToken signup(RegisterData obj);
+
+    @Transactional
+    public Boolean verifyUserEmail(String token);
+
     public UserData findUserByUsername(String username);
     public UserData findUserByFullName(String fullname);
 }
